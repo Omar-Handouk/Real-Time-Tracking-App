6672e4ceb7ee8213e94c017bee5a31a12036d7ab
Omar Ihab Hamed Handouk <o.i.handouk@gmail.com>
2018-10-15 15:36:02 +0200

    Version 0.1.0

--- Public/Javascript/Map.js
+++ Public/Javascript/Map.js
@@ -3,4 +3,49 @@ $(document).ready(() => {
     let update_interval = 2000;
 
     let current_device;
+
+    let map;
+
+    let marker = null;
+
+    $('#FIND').click(function (e) { 
+        e.preventDefault();
+        
+        $('#Locator').empty();
+        
+        current_device = $('#DEV_ID').val();
+
+        $('#Locator').append('<div id="map"></div>');
+        initMap();
+
+        marker = null;
+
+        movePosition();
+    });
+
+    function initMap()
+    {
+        let options = {'center': {'lat': 0, 'lng': 0}, 'zoom': 10};
+        map = new google.maps.Map(document.getElementById('map'), options);
+    }
+
+    function movePosition()
+    {
+        $.get('/GetLocation', {'DEV_ID': current_device}, (response) => {
+
+            let Coordinates = response.Coordinates;
+            let Device = response._id;
+
+            if (marker == null)
+            {
+                marker = new google.maps.Marker({'title': Device, 'position': {'lat': Coordinates.Latitude, 'lng': Coordinates.Longitude}, 'map': map});
+            }
+            else
+            {
+                marker.setPosition(new google.maps.LatLng(Coordinates.Latitude, Coordinates.Longitude));
+            }
+
+            window.setTimeout(movePosition, update_interval);
+        }, 'json');
+    }
 });

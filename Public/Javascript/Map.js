bde80d39950af232cf07c3dd564d7408550e74ec
Omar Ihab Hamed Handouk <o.i.handouk@gmail.com>
2018-10-16 12:58:12 +0200

    Version 0.1.2
    [*] Removed Map APIs from Track.ejs

--- Public/Javascript/Map.js
+++ Public/Javascript/Map.js
@@ -1,6 +1,6 @@
-$(document).ready(() => {
-    
-    let update_interval = 2000;
+$(document).ready(() => { // eslint-disable-line
+
+    let update_interval = 10000;
 
     let current_device;
 
@@ -8,14 +8,14 @@ $(document).ready(() => {
 
     let marker = null;
 
-    $('#FIND').click(function (e) { 
+    $('#FIND').click(function (e) { // eslint-disable-line
         e.preventDefault();
-        
-        $('#Locator').empty();
-        
-        current_device = $('#DEV_ID').val();
 
-        $('#Locator').append('<div id="map"></div>');
+        $('#Locator').empty(); // eslint-disable-line
+
+        current_device = $('#DEV_ID').val(); // eslint-disable-line
+
+        $('#Locator').append('<div id="map"></div>'); // eslint-disable-line
         initMap();
 
         marker = null;
@@ -23,29 +23,43 @@ $(document).ready(() => {
         movePosition();
     });
 
-    function initMap()
-    {
-        let options = {'center': {'lat': 0, 'lng': 0}, 'zoom': 10};
-        map = new google.maps.Map(document.getElementById('map'), options);
+    function initMap() {
+        let options = {
+            'center': {
+                'lat': 21.0,
+                'lng': 0
+            },
+            'zoom': 8
+        };
+        map = new google.maps.Map(document.getElementById('map'), options); // eslint-disable-line
     }
 
-    function movePosition()
-    {
-        $.get('/GetLocation', {'DEV_ID': current_device}, (response) => {
-
+    function movePosition() {
+        $.get('/GetLocation', { // eslint-disable-line
+            'DEV_ID': current_device
+        }, (response) => {
             let Coordinates = response.Coordinates;
             let Device = response._id;
+            let latlng = new google.maps.LatLng(Coordinates.Latitude, Coordinates.Longitude); // eslint-disable-line
 
-            if (marker == null)
-            {
-                marker = new google.maps.Marker({'title': Device, 'position': {'lat': Coordinates.Latitude, 'lng': Coordinates.Longitude}, 'map': map});
-            }
-            else
-            {
-                marker.setPosition(new google.maps.LatLng(Coordinates.Latitude, Coordinates.Longitude));
+            if (marker == null) {
+                marker = new google.maps.Marker({ // eslint-disable-line
+                    'title': Device,
+                    'position': {
+                        'lat': Coordinates.Latitude,
+                        'lng': Coordinates.Longitude
+                    },
+                    'map': map
+                });
+                map.panTo(latlng);
+            } else {
+                marker.setPosition(new google.maps.LatLng(Coordinates.Latitude, Coordinates.Longitude)); // eslint-disable-line
+                map.panTo(latlng);
             }
 
             window.setTimeout(movePosition, update_interval);
         }, 'json');
     }
+
+    $('body').append('<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCj9B_lUMBxSpH4xamTurUOMNi9daqrjsU&callback=initMap" async defer></script>'); // eslint-disable-line
 });
--- Server/Controllers/Location_Handler.js
+++ Server/Controllers/Location_Handler.js
@@ -6,15 +6,18 @@ let ENV = require('../../Miscellaneous/ENVIROMENT_VARIABLES.json') || process.en
 
 exports.getLocation = async function (req, response, next) { // eslint-disable-line
 
-    let DEV_ID = req.body.DEV_ID;
+    let DEV_ID = req.query.DEV_ID;
 
     let cloudantDB_URL = ENV.VCAP_SERVICES.cloudantNoSQLDB[0].credentials.url;
     console.log('Location_Handler.getLocation<Info>: ' + cloudantDB_URL); // eslint-disable-line
 
+    let document_ID = cloudantDB_URL + '/geo_reports/' + DEV_ID;
+    console.log('Location_Handler.getLocation<Info>: ' + document_ID); // eslint-disable-line
+
     let options = {
         'method': 'GET',
         'headers': {'Content-Type': 'application/json'},
-        'url': cloudantDB_URL + '/geo_reports/' + DEV_ID
+        'url': document_ID
     };
 
     request(options, (error, res, body) => {
